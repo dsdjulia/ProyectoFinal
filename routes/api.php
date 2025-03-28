480c336a505adee3055eb0c754572eea5a5ab7b0
GiddonPk <jhg0015@alu.medac.es>
2025-03-28 19:34:08 +0100

    tokens

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiAuthController;
 use App\Http\Controllers\Api\ProductoController;
 
 
 
 // CRUD PRODUCTOS
-Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
-Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show'); 
+
+Route::get('/productos', [ProductoController::class, 'index'])->middleware('auth:sanctum')->name('productos.index');
+Route::get('/productos/{id}', [ProductoController::class, 'show'])->name('productos.show');
 Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');
 Route::patch('/productos/{id}', [ProductoController::class, 'update'])->name('productos.update');
 Route::delete('/productos', [ProductoController::class, 'destroy'])->name('productos.destroy');
 
+Route::post('/login', [ApiAuthController::class, 'login'])->name('login');
--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,12 @@ use App\Http\Controllers\ProductoController;
 
 
 
-Route::get('/dashboard', [ProductoController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [ProductoController::class, 'index'])->middleware('auth')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::prefix('productos')->middleware('auth')->group(function () {
-    Route::get('/total-price', [ProductoController::class, 'index'])->name('productos.total_price');
-    Route::get('/avg-price', [ProductoController::class, 'avg_price'])->name('productos.avg_price');
 });
 
 require __DIR__ . '/auth.php';
