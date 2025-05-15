335ab06e63f29bc4c0151d78092838c2b435050c
cmr0025 <cmr0025@alu.medac.es>
2025-05-15 10:52:39 +0200

    asd

--- app/Http/Controllers/Web/ProveedoresController.php
+++ app/Http/Controllers/Web/ProveedoresController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Web;
 
 use Inertia\Inertia;
 use App\Models\Proveedor;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProveedoresController extends Controller
 {
